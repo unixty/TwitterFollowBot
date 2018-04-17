be26558e4a898a5f74b05aedbc3d42be2cb21efa
unixty <vladimir.utkinx@gmail.com>
2018-04-17 14:01:54 +0300

    changes

--- fol_bot.py
+++ fol_bot.py
@@ -44,25 +44,32 @@ while True:
             if len(not_following_back_bot) < 900:
 
                 print('%sPhase of follow #1%s' % (stars, stars))
-                print(time.asctime(), '\n')
-                my_bot.auto_follow_followers_of_user('vasial9', count = random.randint(80, 110))
+                print(time.asctime(), '\n') #_FireAnt - 100% audit, vasial9 99.5
+                my_bot.auto_follow_followers_of_user('_FireAnt', count = random.randint(180, 210))
 
-                print('%sWaiting 11h%s' % (stars, stars))
-                random.seed() # N0onsense  99.4
-                time.sleep(random.randint(9*60*60, 12*60*60))
+                print('%sWaiting 8.5-10.5h%s' % (stars, stars))
+                print(time.asctime(), '\n')
+                random.seed()
+                time.sleep(random.randint(8.5*60*60, 10.5*60*60))
 
                 print('%sPhase of follow #2%s' % (stars, stars))
-                my_bot.auto_follow_followers_of_user('vasial9', count = random.randint(80, 110))
+                print(time.asctime(), '\n')
+                my_bot.auto_follow_followers_of_user('_FireAnt', count = random.randint(180, 210))
 
-                print('%sWaiting 5h%s' % (stars, stars))
+                print('%sWaiting 5-6h%s' % (stars, stars))
+                print(time.asctime(), '\n')
                 time.sleep(random.randint(5*60*60, 6*60*60))
 
                 print('%sPhase of follow followers%s' % (stars, stars))
+                print(time.asctime(), '\n')
                 my_bot.sync_follows()
                 my_bot.auto_follow_followers()
 
-                print('%sWaiting 3h%s' % (stars, stars))
+
+                print('%sWaiting 3-4h%s' % (stars, stars))
+                print(time.asctime(), '\n')
                 time.sleep(random.randint(3*60*60, 4*60*60))
+
             else:
                 print('%sWaiting 2 days%s' % (stars, stars))
                 print(time.asctime(), '\n')
@@ -87,9 +94,10 @@ while True:
                         out_file.write("%s\n" % (follower))
 
                 my_bot.auto_unfollow_nonfollowers()
-                print('%sWaiting%s' % (stars, stars))
+                print('%sWaiting 17h%s' % (stars, stars))
                 time.sleep(random.randint(15*60*60, 20*60*60))
                 my_bot.sync_follows()
+
 
 
         if command == 'bf':
